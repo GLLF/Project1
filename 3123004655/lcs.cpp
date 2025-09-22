4a8347db2bec0d1101e4ca4330b609176904165e
GLLF <439251693@qq.com>
2025-09-22 21:43:55 +0800

    改为滚动数组

--- 3123004655/lcs.cpp
+++ 3123004655/lcs.cpp
@@ -5,15 +5,15 @@ int computeLCSLength(const std::string& text1, const std::string& text2) {
     int n = text2.length();
     
     // 使用二维向量存储DP表
-    std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
+    std::vector<std::vector<int>> dp(2, std::vector<int>(n + 1, 0));
     
     // 填充DP表
     for (int i = 1; i <= m; i++) {
         for (int j = 1; j <= n; j++) {
             if (text1[i - 1] == text2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
+                dp[i%2][j] = dp[(i - 1)%2][j - 1] + 1;
             } else {
-                dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+                dp[i%2][j] = std::max(dp[(i - 1)%2][j], dp[i%2][j - 1]);
             }
         }
     }
